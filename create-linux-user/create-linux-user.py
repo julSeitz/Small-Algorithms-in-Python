7b9061386dbbac1f70158052fa9a48714060ffc7
Julian Seitz <112495164+julSeitz@users.noreply.github.com>
2025-06-06 16:57:27 +0200

    added script for creation of linux user

---
+++ create-linux-user/create-linux-user.py
@@ -0,0 +1,21 @@
+import subprocess
+
+user_name = "foo"
+user_password = "password1234!"
+
+result = subprocess.run(["cat /etc/passwd | grep {0}".format(user_name)], shell=True, capture_output=True, text=True)
+if len(result.stdout) != 0:
+    print("User {0} already exists!".format(user_name))
+    exit()
+
+result = subprocess.run(["sudo useradd -m " + user_name], shell=True, capture_output=True, text=True)
+if result.returncode == 0:
+    print("Created user " + user_name)
+else:
+    print("An error occured when trying to create user " + user_name)
+
+result = subprocess.run(["echo {0}:{1} | sudo chpasswd".format(user_name, user_password)], shell=True, capture_output=True, text=True)
+if result.returncode == 0:
+    print("Set password for user " + user_name)
+else:
+    print("An error occured when trying to set password for user " + user_name)
